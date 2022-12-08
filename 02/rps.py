6cc18dbc8ed9e52c11c3d3c772f66415e9dc76b4
jbovee <mitouch.b@gmail.com>
2022-12-08 15:04:48 -0500

    Day 2 part two

--- 02/rps.py
+++ 02/rps.py
@@ -5,6 +5,7 @@ def get_input():
 
 def main():
     matches = get_input()
+    # part 1
     score = 0
     points = {
         'A X': 4,
@@ -20,6 +21,23 @@ def main():
     for match in matches:
         score = score + points[match]
     print('Total score: {}'.format(score))
+
+    # part 2
+    score_2 = 0
+    points_2 = {
+        'A X': 3,
+        'A Y': 4,
+        'A Z': 8,
+        'B X': 1,
+        'B Y': 5,
+        'B Z': 9,
+        'C X': 2,
+        'C Y': 6,
+        'C Z': 7
+    }
+    for match in matches:
+        score_2 = score_2 + points_2[match]
+    print('Total score: {}'.format(score_2))
 
 if __name__ == "__main__":
     main()
