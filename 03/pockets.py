832c125770a9511baf67277aedca1410878c3864
jbovee <mitouch.b@gmail.com>
2022-12-08 15:53:16 -0500

    Day 3 part two

--- 03/pockets.py
+++ 03/pockets.py
@@ -7,6 +7,7 @@ def get_input():
 
 def main():
     rucksacks = get_input()
+    # part 1
     total = 0
     for rucksack in rucksacks:
         mid = len(rucksack) // 2
@@ -14,6 +15,14 @@ def main():
         common = set(pocket1).intersection(set(pocket2))
         total = total + priority(list(common)[0])
     print('Total priority: {}'.format(total))
+
+    # part 2
+    total_2 = 0
+    for r in range(0,len(rucksacks),3):
+        r1,r2,r3 = rucksacks[r:r+3]
+        common = set(r1).intersection(set(r2),set(r3))
+        total_2 = total_2 + priority(list(common)[0])
+    print('Total priority: {}'.format(total_2))
 
 if __name__ == '__main__':
     main()
